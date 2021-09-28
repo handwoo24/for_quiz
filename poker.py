783574fb8fff49677c3e25ea6ec1aa007801aba3
handwoo24 <handwoo24@gmail.com>
2021-09-28 22:13:49 +0900

    :hammer: Card 클래스 생성자 정의

--- poker.py
+++ poker.py
@@ -1,11 +1,25 @@
 from __future__ import annotations
 
+import random
+
+
 class Dealer():
 
-    def shuffle():
-        pass
+    def shuffle() -> Card:
+        return Card()
 
 class Card():
+    _shape: str
+    _number: int
 
-    def __init__(self) -> None:
-        pass
+    def __init__(self, shape, number) -> None:
+        self._shape = shape
+        self._number = number
+    
+    @property
+    def shape(self) -> str:
+        return self._shape
+
+    @property
+    def number(self) -> int:
+        return self._number
