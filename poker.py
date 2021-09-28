bae1c32bc2c518f9adee6af15137e8ac3e986911
handwoo24 <handwoo24@gmail.com>
2021-09-28 23:11:26 +0900

    :hammer: hand_out_card, receive_card 메서드 추가

--- poker.py
+++ poker.py
@@ -5,10 +5,12 @@ import random
 class Player():
     _name: str
     _money: int
+    _cards: List[Card]
 
     def __init__(self, name: str, money: int) -> None:
         self._set_name(name)
         self._set_init_money(money)
+        self._cards = []
     
     def _set_name(self, name) -> None:
         self._name = name
@@ -48,6 +50,17 @@ class Player():
         if self.money <= 0:
             return self.action_all_in(self)
         return money
+    
+    @property
+    def cards(self) -> List[Card]:
+        return self._cards
+    
+    @cards.setter
+    def cards(self, cards: List[Card]) -> None:
+        self._cards = cards
+    
+    def receive_card(self, card:Card) -> None:
+        self.cards.append(card)
 
 class Dealer(Player):
 
@@ -64,6 +77,9 @@ class Dealer(Player):
         card_types = ["spade", "heart", "diamond", "clover"]
         cards = [Card(t, i) for t in card_types for i in range(1, 14)]    
         return cards
+    
+    def hand_out_card(self, cards:List[Card]) -> Card:
+        return cards.pop()
 
 class Card():
     _shape: str
@@ -80,15 +96,31 @@ class Card():
     @property
     def number(self) -> int:
         return self._number
+    
 
+dealer = Dealer()
+
+player = Player()
     
-def main():
-    
-    dealer = Dealer()
+def game(dealer: Dealer, player: Player):
+
+    money = 0
 
     cards = dealer.set_up_cards()
     
     dealer.shuffle(cards)
+
+    player.receive_card(dealer.hand_out_card(cards))
+
+    dealer.receive_card(dealer.hand_out_card(cards))
+
+
+
+
+
+
+
+
 
 if __name__ == "__main__":
     main()
