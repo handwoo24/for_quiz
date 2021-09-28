078e62301a9ee8348ab2cf9eac851c2103648ade
handwoo24 <handwoo24@gmail.com>
2021-09-28 22:24:54 +0900

    :hammer: 메서드 수정 및 테스트 출력 생성

--- poker.py
+++ poker.py
@@ -1,15 +1,18 @@
 from __future__ import annotations
-
+from typing import List
 import random
 
 
 class Dealer():
 
-    def shuffle() -> Card:
-        shapes = ["spade", "heart", "diamond", "clover"]
-        card_shape = shapes[random.randint(3)]
-        card_number = random.randrange(1,13)
-        return Card(card_shape, card_number)
+    def shuffle(card_deck) -> List[Card]:
+        random.shuffle(card_deck)
+        return card_deck
+    
+    def set_up_cards(self) -> List[Card]:
+        card_types = ["spade", "heart", "diamond", "clover"]
+        cards = [Card(t, i) for t in card_types for i in range(1, 14)]    
+        return cards
 
 class Card():
     _shape: str
@@ -26,3 +29,11 @@ class Card():
     @property
     def number(self) -> int:
         return self._number
+
+def main():
+    dealer = Dealer()
+    cards = dealer.set_up_cards()
+    print(len(cards))
+
+if __name__ == "__main__":
+    main()
