e5fe597fe6da7c78ea5db809cb4bd64da81d3231
handwoo24 <handwoo24@gmail.com>
2021-09-28 22:52:08 +0900

    :hammer: dealer, player 상속 관계 설정

--- poker.py
+++ poker.py
@@ -2,12 +2,63 @@ from __future__ import annotations
 from typing import List
 import random
 
+class Player():
+    _name: str
+    _money: int
 
-class Dealer():
+    def __init__(self, name: str, money: int) -> None:
+        self._set_name(name)
+        self._set_init_money(money)
+    
+    def _set_name(self, name) -> None:
+        self._name = name
+    
+    def _set_init_money(self) -> None:
+        self.money = 100000
+    
+    @property
+    def name(self) -> str:
+        return self._name
 
-    def shuffle(card_deck) -> List[Card]:
+    @property
+    def money(self) -> int:
+        return self._money
+
+    @money.setter
+    def money(self, money) -> None:
+        self._money = money
+    
+    def action_call(self, money) -> int:
+        if self.money <= money:
+            return self.action_all_in(self)
+        else:
+            self.money -= money
+        return money
+    
+    def action_check(self) -> int:
+        return 0
+    
+    def action_all_in(self) -> int:
+        money = self.money
+        self.money = 0 
+        return money
+    
+    def action_raise(self, money) -> int:
+        self.money -= money
+        if self.money <= 0:
+            return self.action_all_in(self)
+        return money
+
+class Dealer(Player):
+
+    def _set_name(self) -> None:
+        self._name = "jack"
+    
+    def _set_init_money(self) -> None:
+        self.money = 1000000
+
+    def shuffle(card_deck: List[Card]) -> None:
         random.shuffle(card_deck)
-        return card_deck
     
     def set_up_cards(self) -> List[Card]:
         card_types = ["spade", "heart", "diamond", "clover"]
@@ -30,10 +81,14 @@ class Card():
     def number(self) -> int:
         return self._number
 
+    
 def main():
+    
     dealer = Dealer()
+
     cards = dealer.set_up_cards()
-    print(len(cards))
+    
+    dealer.shuffle(cards)
 
 if __name__ == "__main__":
     main()
